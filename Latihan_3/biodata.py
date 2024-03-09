d352186fcac183e34c5900467612b24876ebf4b6
Administrator-SIMRS <rizky.maulana2351@gmail.com>
2024-03-09 15:21:12 +0700

    Penambahan Python Biodata

---
+++ Latihan_3/biodata.py
@@ -0,0 +1,20 @@
+# membuat variabel
+nama = "Rizky"
+semester = 2
+kelas = "TI23B"
+jurusan = "Teknik Informatika" 
+quote = "Tetap Hidup Jangan Mati"
+# membuat boolean huruf depan harus besar --> False bukan false
+married = False
+adalahLakilaki = True
+hobi = ["nonton","main game"]
+print("Nama Saya :", nama)
+print("Semester :", semester)
+print("Kelas :",kelas)
+print("Jurusan :",jurusan)
+print("Quote :", quote)
+print("Sudah menikah :", married)
+print("Jenis Kelamin Laki-Laki :", adalahLakilaki)
+print("Hobi :",hobi)
+
+
